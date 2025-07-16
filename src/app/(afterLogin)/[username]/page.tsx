8193044030d4414a57a8326e5d0ae5be238edb6f
yourangrang <163999418+yourangrang@users.noreply.github.com>
2025-07-16 19:02:51 +0900

    profile page, backbutton

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,8 +1,37 @@
+import style from './profile.module.css';
+import Post from "@/app/(afterLogin)/_component/Post";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
 
 export default function Profile() {
+  const user = { // 임시
+    id: 'yourang',
+    nickname: '유랑',
+    image: '/yourang.png',
+  };
+
   return (
-    <main>
-      프로필
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>{user.id}</div>
+        </div>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </main>
   )
 }
