972c172a819fea2b03327b5384e6f33a6675ec75
yourangrang <163999418+yourangrang@users.noreply.github.com>
2025-09-18 18:52:50 +0900

    게시물 업로드

--- src/app/(afterLogin)/home/_component/FollowingPosts.tsx
+++ src/app/(afterLogin)/home/_component/FollowingPosts.tsx
@@ -1,19 +1,55 @@
-"use client"
+'use client';
 
-import {useSuspenseQuery} from "@tanstack/react-query";
-import {getFollowingPosts} from "@/app/(afterLogin)/home/_lib/getFollowingPosts";
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
+import { getFollowingPosts } from '../_lib/getFollowingPosts';
 import Post from "@/app/(afterLogin)/_component/Post";
-import { Post as IPost } from '@/model/Post';
+import {Post as IPost} from "@/model/Post"
+import { useInView } from 'react-intersection-observer';
+import { Fragment, useEffect } from 'react';
 
 export default function FollowingPosts() {
-  const { data, isPending } = useSuspenseQuery<IPost[]>({
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
+    IPost[],
+    Object,
+    IPost[],
+    [_1: string, _2: string],
+    number
+  >({
     queryKey: ['posts', 'followings'],
     queryFn: getFollowingPosts,
-    staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+    initialPageParam: 0, // required
+    getNextPageParam: (lastPage) => {
+  if (Array.isArray(lastPage) && lastPage.length > 0) {
+    return lastPage[lastPage.length - 1]?.postId;
+  }
+  return undefined;
+},
+    staleTime: 60 * 1000,
     gcTime: 300 * 1000,
-  })
+  });
 
-  return data?.map((post) => (
-    <Post key={post.postId} post={post} />
-  ))
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0,
+  });
+
+  useEffect(() => {
+    //. inView: ref가 화면에 보일 때
+    //. !isFetching: 패칭상태 아닐때 (중복 패칭 방지)
+    //. hasNextPage: 다음 페이지가 있을 때
+    console.log('useEffect', { inView, isFetching, hasNextPage });
+    if (inView && !isFetching && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
+
+  return data?.pages.map((page, index) => (
+    <Fragment key={`posts-followings-page-${index}`}>
+      {page.map((post) => (
+        <Post key={post.postId} post={post} />
+      ))}
+      {!isFetching && <div ref={ref} style={{ height: 50 }}></div>}
+      {isFetching && <div style={{ height: 50 }}></div>}
+    </Fragment>
+  ));
 }
--- src/app/(afterLogin)/home/_component/PostForm.tsx
+++ src/app/(afterLogin)/home/_component/PostForm.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import {ChangeEventHandler, FormEventHandler, useRef, useState} from "react";
+import {ChangeEventHandler, FormEvent, FormEventHandler, useRef, useState} from "react";
 import style from './postForm.module.css';
 import {Session} from "next-auth";
 import TextareaAutosize from 'react-textarea-autosize';
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {Post} from "@/model/Post";
 
 type Props = {
   me: Session | null;
@@ -12,26 +14,59 @@ export default function PostForm({ me }: Props) {
   const imageRef = useRef<HTMLInputElement>(null);
   const [preview, setPreview] = useState<Array<{ dataUrl: string, file: File } | null>>([]);
   const [content, setContent] = useState('');
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (e: FormEvent) => {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append('content', content);
+      preview.forEach((p) => {
+        if (p) {
+          formData.append('images', p.file);
+        }
+      })
+      return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, {
+        method: 'post',
+        credentials: 'include',
+        body: formData,
+      });
+    },
+    async onSuccess(response, variable, context) {
+      const newPost = await response.json();
+      setContent('');
+      setPreview([]);
+      if (queryClient.getQueryData(['posts', 'recommends'])) {
+        queryClient.setQueryData(['posts', 'recommends'], (prevData: { pages: Post[][] }) => {
+          const shallow = {
+            ...prevData,
+            pages: [...prevData.pages],
+          };
+          shallow.pages[0] = [...shallow.pages[0]];
+          shallow.pages[0].unshift(newPost);
+          return shallow;
+        })
+      }
+      if (queryClient.getQueryData(['posts', 'followings'])) {
+        queryClient.setQueryData(['posts', 'followings'], (prevData: { pages: Post[][] }) => {
+          const shallow = {
+            ...prevData,
+            pages: [...prevData.pages],
+          };
+          shallow.pages[0] = [...shallow.pages[0]];
+          shallow.pages[0].unshift(newPost);
+          return shallow;
+        })
+      }
+    },
+    onError(error) {
+      console.error(error);
+      alert('업로드 중 에러가 발생했습니다.');
+    },
+  });
 
   const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setContent(e.target.value);
-  }
-
-  const onSubmit: FormEventHandler = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('content', content);
-    preview.forEach((p) => {
-      if (p) {
-        formData.append('images', p.file);
-      }
-    })
-
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, {
-      method: 'post',
-      credentials: 'include',
-      body: formData,
-    })
   }
 
   const onClickButton = () => {
@@ -67,7 +102,7 @@ export default function PostForm({ me }: Props) {
   };
 
   return (
-    <form className={style.postForm} onSubmit={onSubmit}>
+    <form className={style.postForm} onSubmit={mutation.mutate}>
       <div className={style.postUserSection}>
         <div className={style.postUserImage}>
           <img src={me?.user?.image as string} alt={me?.user?.email as string} />
